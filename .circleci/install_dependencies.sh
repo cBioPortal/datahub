a8a7ef30eb6f3e45930acced6bc7f06de5f2042d
Sander Tan <sandertan@thehyve.nl>
2018-10-01 17:46:49 +0200

    CI test for seed database

--- .circleci/install_dependencies.sh
+++ .circleci/install_dependencies.sh
@@ -3,10 +3,10 @@
 
 # Install python dependencies
 cd ~/repo/.circleci
-sudo pip install -r requirements_py
+sudo pip install -r requirements.txt
 
 # Install and configure Git LFS
-cd ~/repo
+cd ~/
 wget https://github.com/git-lfs/git-lfs/releases/download/v2.3.4/git-lfs-linux-amd64-2.3.4.tar.gz
 tar -xvf git-lfs-linux-amd64-2.3.4.tar.gz
 cd git-lfs-2.3.4
@@ -16,9 +16,10 @@ sudo chown -R circleci .git
 git lfs install --skip-smudge
 
 # Clone datahub master branch
+cd ~/
 git clone --depth 1 -b master https://github.com/cbioportal/cbioportal.git
 
 # Make test reports location
-cd ~/repo/.circleci
-mkdir ~/repo/test-reports
-mkdir ~/repo/test-reports/ERRORS
+cd ~/
+mkdir ~/test-reports
+mkdir ~/test-reports/ERRORS
