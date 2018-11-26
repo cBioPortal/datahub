5759da5c4e4747113084e043a10e9f44f0bb6317
Sander Tan <sandertan@thehyve.nl>
2018-11-26 11:35:50 +0100

    Fix validate all studies command

--- .circleci/validate_all_studies.sh
+++ .circleci/validate_all_studies.sh
@@ -10,8 +10,8 @@ git lfs pull -I "public"
 
 num_studies=${#list_of_study_dirs[@]}
 
-test_reports_location="$HOME/repo/test-reports"
-validation_command="$HOME/repo/cbioportal/core/src/main/scripts/importer/./validateStudies.py -d $HOME/repo/public/ -p $HOME/repo/.circleci/portalinfo -html $test_reports_location"
+test_reports_location="$HOME/test-reports"
+validation_command="$HOME/cbioportal/core/src/main/scripts/importer/./validateStudies.py -d $HOME/repo/public/ -p $HOME/repo/.circleci/portalinfo -html $test_reports_location"
 echo $'\nExecuting: '; echo $validation_command
 if sh -c "$validation_command" ; then
     echo "Tests passed successfully"
