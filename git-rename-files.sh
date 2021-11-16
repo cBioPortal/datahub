361ffa01f6fe08abe02300a4212b6ffe2655428f
Ramya Madupuri <ramyamadupuri99@gmail.com>
2021-11-16 12:16:27 -0500

    Rename meta_mrna_microarray.txt to meta_mrna_affymetrix_microarray.txt

---
+++ git-rename-files.sh
@@ -0,0 +1,21 @@
+##################
+git mv /Users/rmadupuri/GitHub/datahub/public/mbn_mdacc_2013/meta_mrna_microarray.txt /Users/rmadupuri/GitHub/datahub/public/mbn_mdacc_2013/meta_mrna_affymetrix_microarray.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/ov_tcga/meta_mrna_microarray.txt /Users/rmadupuri/GitHub/datahub/public/ov_tcga/meta_mrna_affymetrix_microarray.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/gbm_tcga/meta_mrna_microarray.txt /Users/rmadupuri/GitHub/datahub/public/gbm_tcga/meta_mrna_affymetrix_microarray.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/lusc_tcga/meta_mrna_microarray.txt /Users/rmadupuri/GitHub/datahub/public/lusc_tcga/meta_mrna_affymetrix_microarray.txt
+git add .
+git commit -m "Rename meta_mrna_microarray.txt to meta_mrna_affymetrix_microarray.txt"
+##################
+git mv /Users/rmadupuri/GitHub/datahub/public/ov_tcga/meta_mrna_microarray_zscores_ref_diploid_samples.txt /Users/rmadupuri/GitHub/datahub/public/ov_tcga/meta_mrna_affymetrix_microarray_zscores_ref_diploid_samples.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/gbm_tcga/meta_mrna_microarray_zscores_ref_diploid_samples.txt /Users/rmadupuri/GitHub/datahub/public/gbm_tcga/meta_mrna_affymetrix_microarray_zscores_ref_diploid_samples.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/lusc_tcga/meta_mrna_microarray_zscores_ref_diploid_samples.txt /Users/rmadupuri/GitHub/datahub/public/lusc_tcga/meta_mrna_affymetrix_microarray_zscores_ref_diploid_samples.txt
+git add .
+git commit -m "Rename meta_mrna_microarray_zscores_ref_diploid_samples.txt to meta_mrna_affymetrix_microarray_zscores_ref_diploid_samples.txt"
+##################
+git mv /Users/rmadupuri/GitHub/datahub/public/mbn_mdacc_2013/meta_mrna_microarray_zscores_ref_all_samples.txt /Users/rmadupuri/GitHub/datahub/public/mbn_mdacc_2013/meta_mrna_affymetrix_microarray_zscores_ref_all_samples.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/ov_tcga/meta_mrna_microarray_zscores_ref_all_samples.txt /Users/rmadupuri/GitHub/datahub/public/ov_tcga/meta_mrna_affymetrix_microarray_zscores_ref_all_samples.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/gbm_tcga/meta_mrna_microarray_zscores_ref_all_samples.txt /Users/rmadupuri/GitHub/datahub/public/gbm_tcga/meta_mrna_affymetrix_microarray_zscores_ref_all_samples.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/lusc_tcga/meta_mrna_microarray_zscores_ref_all_samples.txt /Users/rmadupuri/GitHub/datahub/public/lusc_tcga/meta_mrna_affymetrix_microarray_zscores_ref_all_samples.txt
+git rm git-rename-files.sh
+git add .
+git commit -m "Rename meta_mrna_microarray_zscores_ref_all_samples.txt to meta_mrna_affymetrix_microarray_zscores_ref_all_samples.txt"
