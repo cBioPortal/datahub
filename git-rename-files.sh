00f630f41ccd513542a8b67a1b7c1cdb2e6a72e3
Ramya Madupuri <ramyamadupuri99@gmail.com>
2021-11-16 11:05:45 -0500

    Rename data_CNA.txt to data_cna.txt

---
+++ git-rename-files.sh
@@ -0,0 +1,58 @@
+git mv /Users/rmadupuri/GitHub/datahub/public/brca_cptac_2020/data_CNA.txt /Users/rmadupuri/GitHub/datahub/public/brca_cptac_2020/data_cna.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/brca_mapk_hp_msk_2021/data_CNA.txt /Users/rmadupuri/GitHub/datahub/public/brca_mapk_hp_msk_2021/data_cna.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/gbm_cptac_2021/data_CNA.txt /Users/rmadupuri/GitHub/datahub/public/gbm_cptac_2021/data_cna.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/crc_nigerian_2020/data_CNA.txt /Users/rmadupuri/GitHub/datahub/public/crc_nigerian_2020/data_cna.txt
+git add .
+git commit -m "Rename data_CNA.txt to data_cna.txt"
+##################
+git mv /Users/rmadupuri/GitHub/datahub/public/brain_cptac_2020/data_linear_CNA.txt /Users/rmadupuri/GitHub/datahub/public/brain_cptac_2020/data_linear_cna.txt
+git add .
+git commit -m "Rename data_linear_CNA.txt to data_linear_cna.txt"
+##################
+git mv /Users/rmadupuri/GitHub/datahub/public/brca_cptac_2020/data_log2CNA.txt /Users/rmadupuri/GitHub/datahub/public/brca_cptac_2020/data_log2_cna.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/gbm_cptac_2021/data_log2CNA.txt /Users/rmadupuri/GitHub/datahub/public/gbm_cptac_2021/data_log2_cna.txt
+git add .
+git commit -m "Rename data_log2CNA.txt to data_log2_cna.txt"
+##################
+git mv /Users/rmadupuri/GitHub/datahub/public/brca_mapk_hp_msk_2021/data_gene_matrix.txt /Users/rmadupuri/GitHub/datahub/public/brca_mapk_hp_msk_2021/data_gene_panel_matrix.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/crc_nigerian_2020/data_gene_matrix.txt /Users/rmadupuri/GitHub/datahub/public/crc_nigerian_2020/data_gene_panel_matrix.txt
+git add .
+git commit -m "Rename data_gene_matrix.txt to data_gene_panel_matrix.txt"
+##################
+git mv /Users/rmadupuri/GitHub/datahub/public/brca_mapk_hp_msk_2021/data_SV.txt /Users/rmadupuri/GitHub/datahub/public/brca_mapk_hp_msk_2021/data_sv.txt
+git add .
+git commit -m "Rename data_SV.txt to data_sv.txt"
+##################
+git mv /Users/rmadupuri/GitHub/datahub/public/brain_cptac_2020/data_protein_quantification_Zscores.txt /Users/rmadupuri/GitHub/datahub/public/brain_cptac_2020/data_protein_quantification_zscores.txt
+git add .
+git commit -m "Rename data_protein_quantification_Zscores.txt to data_protein_quantification_zscores.txt"
+##################
+git mv /Users/rmadupuri/GitHub/datahub/public/brca_cptac_2020/meta_CNA.txt /Users/rmadupuri/GitHub/datahub/public/brca_cptac_2020/meta_cna.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/brca_mapk_hp_msk_2021/meta_CNA.txt /Users/rmadupuri/GitHub/datahub/public/brca_mapk_hp_msk_2021/meta_cna.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/gbm_cptac_2021/meta_CNA.txt /Users/rmadupuri/GitHub/datahub/public/gbm_cptac_2021/meta_cna.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/crc_nigerian_2020/meta_CNA.txt /Users/rmadupuri/GitHub/datahub/public/crc_nigerian_2020/meta_cna.txt
+git add .
+git commit -m "Rename meta_CNA.txt to meta_cna.txt"
+##################
+git mv /Users/rmadupuri/GitHub/datahub/public/brain_cptac_2020/meta_linear_CNA.txt /Users/rmadupuri/GitHub/datahub/public/brain_cptac_2020/meta_linear_cna.txt
+git add .
+git commit -m "Rename meta_linear_CNA.txt to meta_linear_cna.txt"
+##################
+git mv /Users/rmadupuri/GitHub/datahub/public/brca_cptac_2020/meta_log2CNA.txt /Users/rmadupuri/GitHub/datahub/public/brca_cptac_2020/meta_log2_cna.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/gbm_cptac_2021/meta_log2CNA.txt /Users/rmadupuri/GitHub/datahub/public/gbm_cptac_2021/meta_log2_cna.txt
+git add .
+git commit -m "Rename meta_log2CNA.txt to meta_log2_cna.txt"
+##################
+git mv /Users/rmadupuri/GitHub/datahub/public/brca_mapk_hp_msk_2021/meta_gene_matrix.txt /Users/rmadupuri/GitHub/datahub/public/brca_mapk_hp_msk_2021/meta_gene_panel_matrix.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/crc_nigerian_2020/meta_gene_matrix.txt /Users/rmadupuri/GitHub/datahub/public/crc_nigerian_2020/meta_gene_panel_matrix.txt
+git add .
+git commit -m "Rename meta_gene_matrix.txt to meta_gene_panel_matrix.txt"
+##################
+git mv /Users/rmadupuri/GitHub/datahub/public/sarc_mskcc/meta_mRNA_outliers.txt /Users/rmadupuri/GitHub/datahub/public/sarc_mskcc/meta_mrna_outliers.txt
+git mv /Users/rmadupuri/GitHub/datahub/public/prad_mskcc/meta_mRNA_outliers.txt /Users/rmadupuri/GitHub/datahub/public/prad_mskcc/meta_mrna_outliers.txt
+git add .
+git commit -m "Rename meta_mRNA_outliers.txt to meta_mrna_outliers.txt"
+##################
+git mv /Users/rmadupuri/GitHub/datahub/public/brain_cptac_2020/meta_protein_quantification_Zscores.txt /Users/rmadupuri/GitHub/datahub/public/brain_cptac_2020/meta_protein_quantification_zscores.txt
+git add .
+git commit -m "Rename meta_protein_quantification_Zscores.txt to meta_protein_quantification_zscores.txt"
