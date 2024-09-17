4d9d687e1454615e960667d8acc8a310d37a87ec
rmadupuri <ramyamadupuri99@gmail.com>
2024-09-17 17:21:18 -0400

    test

--- .circleci/validate_changed_studies.sh
+++ .circleci/validate_changed_studies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # This script detects the studies that were changed and triggers the validation accordingly
 
-STUDIES_DIR="public/"
+STUDIES_DIR=("public/" "crdc/gdc/")
 REPO_DIR="$HOME/repo/"
 TEST_REPORTS_LOCATION="$HOME/test-reports"
 ERRORS_DIR="$TEST_REPORTS_LOCATION/ERRORS"
@@ -17,26 +17,28 @@ list_of_study_dirs=()
 
 for file_changing in $files_changing; do
     # if file is part of studies_dir, store its directory path (except case_lists)
-    if [[ $file_changing = *$STUDIES_DIR* ]] && [[ $file_changing != *".htm"* ]]; then
-      echo "study file changing > [$file_changing]"
-      dir_name=$(dirname $file_changing)
-      # match case_list*, caselist* as a case list dir (actually only case_lists is valid, 
-      # but this is up to validation script to flag):
-      if [[ $dir_name != *"/case_list"* ]] && [[ $dir_name != *"/caselist"* ]] && [[ $dir_name != *"/archived_files"* ]] && [[ $dir_name != *"/gene_sets"* ]] && [[ $dir_name != *"/normals"* ]]; then
-        echo "study dir > [$dir_name]"
-      else
-        # get parent dir:
-        dir_name=`dirname $dir_name`
-        echo "study dir > [$dir_name]"
+    for STUDIES_DIR in "${STUDIES_DIRS[@]}"; do
+      if [[ $file_changing = *$STUDIES_DIR* ]] && [[ $file_changing != *".htm"* ]]; then
+        echo "study file changing > [$file_changing]"
+        dir_name=$(dirname $file_changing)
+        # match case_list*, caselist* as a case list dir (actually only case_lists is valid, 
+        # but this is up to validation script to flag):
+        if [[ $dir_name != *"/case_list"* ]] && [[ $dir_name != *"/caselist"* ]] && [[ $dir_name != *"/archived_files"* ]] && [[ $dir_name != *"/gene_sets"* ]] && [[ $dir_name != *"/normals"* ]] && [[ $dir_name != *"/validation_reports"* ]]; then
+          echo "study dir > [$dir_name]"
+        else
+          # get parent dir:
+          dir_name=`dirname $dir_name`
+          echo "study dir > [$dir_name]"
+        fi
+        if [[ ! " ${list_of_study_dirs[@]} " =~ " $dir_name " ]]; then
+          echo "adding to list..."
+          list_of_study_dirs+=("$dir_name")
+          echo "downloading files from git lfs..."
+          git lfs pull -I "$dir_name/*"
+          git lfs pull -I "$dir_name/case_lists/*"
+        fi
       fi
-      if [[ ! " ${list_of_study_dirs[@]} " =~ " $dir_name " ]]; then
-        echo "adding to list..."
-        list_of_study_dirs+=("$dir_name")
-        echo "downloading files from git lfs..."
-        git lfs pull -I "$dir_name/*"
-        git lfs pull -I "$dir_name/case_lists/*"
-      fi
-    fi
+    done
 done
 num_studies=${#list_of_study_dirs[@]}
 if [[ $num_studies > 0 ]]; then
