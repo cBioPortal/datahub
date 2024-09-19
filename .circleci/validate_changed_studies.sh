d91d670f967d4f9157ac597fc1008abdd4ccfcd3
rmadupuri <ramyamadupuri99@gmail.com>
2024-09-19 11:51:39 -0400

    test

--- .circleci/validate_changed_studies.sh
+++ .circleci/validate_changed_studies.sh
@@ -5,6 +5,7 @@ STUDIES_DIRS=("public/" "crdc/gdc/")
 REPO_DIR="$HOME/repo/"
 TEST_REPORTS_LOCATION="$HOME/test-reports"
 ERRORS_DIR="$TEST_REPORTS_LOCATION/ERRORS"
+LOG_DIR="$TEST_REPORTS_LOCATION/logs"
 VALIDATION_SCRIPT="$HOME/cbioportal-core/scripts/importer/validateStudies.py"
 GIT_REMOTE_URL="git@github.com:cbioportal/datahub.git"
 MAX_THREADS=7
@@ -12,10 +13,9 @@ MAX_THREADS=7
 git remote add upstream "$GIT_REMOTE_URL"
 git fetch upstream master
 
-echo "Changed files according to git diff:"
+mkdir -p "$LOG_DIR"
+
 files_changing=$(git diff --name-only --diff-filter=ACMRU upstream/master)
-echo "$files_changing"
-echo "------------------------------"
 list_of_study_dirs=()
 
 for file_changing in $files_changing; do
@@ -59,18 +59,19 @@ if [[ $num_studies > 0 ]]; then
       # append sleep command between commands
       ((num=num+1))
       mod=$(($num % $break_num))
+      log_file="$LOG_DIR/$(basename $study).log"
       # if [ $mod = 0 ] ; then
       #   validation_command="${validation_command} && sleep $((num*2))"
       # fi
       # append the first study
       if [[ -z "$validation_command" ]] ; then
-        validation_command="($VALIDATION_SCRIPT -d $REPO_DIR -l $study -p $REPO_DIR/.circleci/portalinfo -html $TEST_REPORTS_LOCATION/$study"
+        validation_command="($VALIDATION_SCRIPT -d $REPO_DIR -l $study -p $REPO_DIR/.circleci/portalinfo -html $TEST_REPORTS_LOCATION/$study  > $log_file 2>&1"
       else
         # run each validation individually in the background
         if [ $mod = 0 ] ; then
-          validation_command="${validation_command}) & ($VALIDATION_SCRIPT -d $REPO_DIR -l $study -p $REPO_DIR/.circleci/portalinfo -html $TEST_REPORTS_LOCATION/$study"
+          validation_command="${validation_command}) & ($VALIDATION_SCRIPT -d $REPO_DIR -l $study -p $REPO_DIR/.circleci/portalinfo -html $TEST_REPORTS_LOCATION/$study > $log_file 2>&1"
         else
-          validation_command="${validation_command} ; $VALIDATION_SCRIPT -d $REPO_DIR -l $study -p $REPO_DIR/.circleci/portalinfo -html $TEST_REPORTS_LOCATION/$study"
+          validation_command="${validation_command} ; $VALIDATION_SCRIPT -d $REPO_DIR -l $study -p $REPO_DIR/.circleci/portalinfo -html $TEST_REPORTS_LOCATION/$study > $log_file 2>&1"
         fi
       fi
   done
@@ -81,7 +82,7 @@ if [[ $num_studies > 0 ]]; then
   while true; do
     wait -n || {
       code="$?"
-      echo "waiting for all processes to finish..."
+      echo -e "waiting for all processes to finish...\n\n"
       # exit only when all processes finished
       if (( code = 127 )); then
         break
@@ -89,8 +90,15 @@ if [[ $num_studies > 0 ]]; then
     }
   done
   
+  for log in "$LOG_DIR"/*.log; do
+    if [[ -f "$log" ]]; then
+      cat "$log"
+      echo -e "\n----------------------------------------------------\n"
+    fi
+  done
+  
   # find all studies with error
-  erred_studies=$(grep -rl "$TEST_REPORTS_LOCATION" -e 'Validation status.*Failed')
+  erred_studies=$(pcregrep -rMl 'Validation status:\n\s*<strong>\s*Failed' "$TEST_REPORTS_LOCATION")
   if [[ $? -eq 0 ]] && [[ -n "$erred_studies" ]]; then
     echo $'\n====List of error studies:====\n'
     echo "$erred_studies"
